93253c6ee68da791ef446a6b75b83d579558a647
Dillion <99517379+Dillion101@users.noreply.github.com>
2023-11-16 19:08:56 +0000

    Update war.js

--- war.js
+++ war.js
@@ -1,5 +1,5 @@
 // This is recursive function that takes in an integer and splits it into individual characters, adds them one by one and then produces an answer
-// If this answer is also a multiple character it calls the function again. This is due to the recursive nature of the code.
+// If this answer is also a multiple character(i.e equal to or greater than 10) it calls the function again. This is due to the recursive nature of the code.
 // For example, digitalRoot(577) will split 577 into 5+5+7 = 17. 17 will further be split into 1+7=8. Thus the answer is 8.
 
 function digitalRoot(num){
